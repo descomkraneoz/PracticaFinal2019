f7e9ebea7df415c859efbc36744cadc76bd8f8c2
descomkraneoz <descom666999@gmail.com>
2019-05-17 13:02:56 +0200

    sexto commit, trabajando en ventanas

--- src/VentanaInicio.java
+++ src/VentanaInicio.java
@@ -3,30 +3,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VentanaInicio {
-    private JPanel panel1;
     private JTextField textField1;
-    private JButton verificarButton;
-    private JButton nuevoUsuarioButton;
-    private JButton salirButton;
     private JPasswordField passwordField1;
+    private JButton aceptarButton;
+    private JButton nuevoUsuarioButton;
+    private JButton cancelarButton;
+
 
     public VentanaInicio() {
-        salirButton.addActionListener(new ActionListener() {
+        aceptarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("He pulsado salir");
+                System.out.println("Has pulsado aceptar");
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame=new JFrame("VentanaInicio");
-        frame.setContentPane(new VentanaInicio().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+        VentanaInicio miVentana = new VentanaInicio();
+
 
     }
 }
