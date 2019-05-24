17c209c0f17cfc3eef430a594917179f155fd918
descomkraneoz <descom666999@gmail.com>
2019-05-24 12:55:40 +0200

    8mo commit, trabajando en ventanas

---
+++ src/MarcoUsuario.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MarcoUsuario extends JFrame {
+
+    public MarcoUsuario() {
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Ventana de Usuario");
+        setVisible(true);
+    }
+
+    public void centrarMarco() {
+        Toolkit miPantallaCentrada = Toolkit.getDefaultToolkit();
+        Dimension tamanyoPantalla = miPantallaCentrada.getScreenSize();
+        int alturaPantalla = tamanyoPantalla.height;
+        int anchoPantalla = tamanyoPantalla.width;
+        setSize(anchoPantalla / 2, alturaPantalla / 2);
+        setLocation(anchoPantalla / 4, alturaPantalla / 4);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        PanelUsuario miPanel = new PanelUsuario();
+        MarcoUsuario miMarco = new MarcoUsuario();
+        miMarco.add(miPanel);
+        miMarco.centrarMarco();
+
+
+    }
+
+}
--- src/PanelUsuario.java
+++ src/PanelUsuario.java
@@ -1,44 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class PanelUsuario extends JFrame {
-
+public class PanelUsuario extends JPanel {
     JLabel password = new JLabel("Password");
     JPasswordField passwordField = new JPasswordField();
     JButton nuevoUsuario = new JButton("Crear nuevo Usuario");
-    GridLayout filasColumnas = new GridLayout(2, 3);
+    GridBagLayout gbl = new GridBagLayout();
 
     public PanelUsuario() {
-        setSize(500, 500);
-        setVisible(true);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Ventana de Usuario");
-        filasColumnas.setColumns(2);
-        filasColumnas.setRows(2);
-        add(password);
-        add(passwordField);
-        add(nuevoUsuario);
 
     }
 
-    public void centraPantalla() {
-        Toolkit miPantallaCentrada = Toolkit.getDefaultToolkit();
-        Dimension tamanyoPantalla = miPantallaCentrada.getScreenSize();
-        int alturaPantalla = tamanyoPantalla.height;
-        int anchoPantalla = tamanyoPantalla.width;
-        setSize(anchoPantalla / 2, alturaPantalla / 2);
-        setLocation(anchoPantalla / 4, alturaPantalla / 4);
-        setVisible(true);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawString("Bienvenido al panel de usuario", 200, 200);
     }
-
 
     public static void main(String[] args) {
-        PanelUsuario mi = new PanelUsuario();
-        //mi.centraPantalla();
-        //mi.pack();
+        PanelUsuario miPanel = new PanelUsuario();
+        MarcoUsuario miMarco = new MarcoUsuario();
+        miMarco.add(miPanel);
+        miMarco.centrarMarco();
+
     }
+
 }
